2faefdc43b1025956184b655eba187c23e179de9
DS <ds@ds.dev>
2026-02-06 14:11:54 -0800

    updated site

--- styles/EducationStyles.js
+++ styles/EducationStyles.js
@@ -5,7 +5,7 @@ export const EducationDiv = styled.div`
   align-items: center;
   display: flex;
   flex-direction: column;
-  font-size: 1.12rem;
+  font-size: 1.1rem;
   justify-content: center;
   margin: 50px auto 0 auto;
   width: 80%;
